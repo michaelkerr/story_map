0e13dff68b3d4c5b359f4fb488c8159efd80715c
Michael Kerr <michael@id.me>
2015-04-17 08:39:47 -0400

    Initial commit

---
+++ story_map.rb
@@ -0,0 +1,23 @@
+# story_map.rb
+
+require 'sinatra'
+require 'whenever'
+
+
+# get '/jira' do
+# 	'updating from jira'
+# end
+
+# get '/trello' do
+# 	'updating from trello'
+# end
+
+
+#check for new jira cards, largest jira key
+#if no component, put in incoming
+
+#check for closed/deleted cards - store all keys for trello?
+
+#which one wins? FIFO?
+
+
